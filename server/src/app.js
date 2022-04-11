2cf57db1d2f880a8a6a1731d70c6d6a9b75a74b9
shin <heoconlinhat123@gmail.com>
2022-04-12 06:47:31 +0700

    Add morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const smartphonesRouter = require('@routes/smartphones/smartphones.router');
 
 const app = express();
-
 app.use(express.json());
+app.use(morgan('combined'));
 
 app.use('/api/v1/smartphones', smartphonesRouter);
 
