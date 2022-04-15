e104c1ee2e6eb2ebe1e850860976be5c98919205
shin <heoconlinhat123@gmail.com>
2022-04-15 21:14:04 +0700

    Add views/frontend for testing api

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,35 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const { engine } = require('express-handlebars');
 
 const vTestApi = require('@routes/v0.2.api');
 const authRouter = require('@routes/auth/auth.router');
 
 const app = express();
-app.use(express.json());
-app.use(morgan('combined'));
-app.set('json spaces', 2);
 
+app.set('json spaces', 2);
+app.set('view engine', 'hbs');
+app.set('views', path.resolve('src/views'));
+app.engine(
+  'hbs',
+  engine({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: path.resolve('src/views/layouts'),
+    partialsDir: path.resolve('src/views/partials'),
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.static(path.resolve('src/public')));
+app.use(morgan('combined'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
 app.use(authRouter);
 app.use('/api', vTestApi);
 
