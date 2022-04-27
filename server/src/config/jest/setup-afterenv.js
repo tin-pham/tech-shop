4589c28afe76f18d4e01b17fe7c1861f3b5b629c
shin <heoconlinhat123@gmail.com>
2022-04-27 19:38:49 +0700

    fix ci

--- server/src/config/jest/setup-afterenv.js
+++ server/src/config/jest/setup-afterenv.js
@@ -5,9 +5,9 @@ const { deleteAllTestUsers } = require('@models/users/users.model');
 beforeAll(async () => {
   console.log(process.env.MONGO_URL);
   await mongoConnect();
-});
+}, 30000);
 
 afterAll(async () => {
   await deleteAllTestUsers();
   await mongoDisconnect();
-});
+}, 30000);
